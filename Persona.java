e0336cca7b88cccde6edf4eaea3bb7b16fcd5e7a
Jose <josedavidcucanchonpp@gmail.com>
2025-03-28 09:33:15 -0500

    hola

--- Persona.java
+++ Persona.java
@@ -2,4 +2,11 @@ public class Persona {
     private String cedula;
     private String nombre;
     private String apellido;
+
+
+    public Persona(String cedula, String nombre, String apellido) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
 }
